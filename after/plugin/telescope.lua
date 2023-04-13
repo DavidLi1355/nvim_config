6fa395579ba27ed46a770148c4437a7eb6f9c371
David Li <lid1355218215@outlook.com>
2023-04-12 21:19:31 -0500

    Use telescope for reference search and current buffer search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -7,4 +7,11 @@ wk.register({
         s = { builtin.live_grep, "Live Grep" },
         b = { builtin.buffers, "Buffers" },
     },
+    vim.keymap.set('n', '<leader>/', function()
+        -- You can pass additional configuration to telescope to change theme, layout, etc.
+        require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+            winblend = 10,
+            previewer = false,
+        })
+    end, { desc = 'Fuzzily search in current buffer' })
 }, { prefix = "<leader>" })
